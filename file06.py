60fe35c8605c3ead8c1e730d9ee438f81c9859d8
rustambekjumaqulov@gmail.com <rustambekjumaqulov@gmail.com>
2022-11-09 21:38:52 +0500

    '06'

--- file06.py
+++ file06.py
@@ -6,5 +6,10 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    ans = []
+    for i in data:
+        ans.append(len(i))
+    return ans
+f = open('txt_file/data06.txt').read().split()
+print(main(f))
 # Read data from file
