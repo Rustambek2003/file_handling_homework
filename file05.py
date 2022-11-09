960eee0adfa4e7d97c5c5dc82fa1145136ef678c
rustambekjumaqulov@gmail.com <rustambekjumaqulov@gmail.com>
2022-11-09 21:35:49 +0500

    '05'

--- file05.py
+++ file05.py
@@ -6,5 +6,14 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    n,k = 0,0
+    for i in data:
+        if i.isdigit():
+            n += 1
+        else:
+            k += 1
+    ans = [n,k]
+    return ans
+f = open('txt_file/data05.txt').read()
+print(main(f))
 # Read data from file
