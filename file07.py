f533ae158bcf422ff098098a3d2575ed1675042e
rustambekjumaqulov@gmail.com <rustambekjumaqulov@gmail.com>
2022-11-09 22:06:26 +0500

    '07'

--- file07.py
+++ file07.py
@@ -6,5 +6,12 @@ def main(data:str):
     Returns:
         int: return answer
     """
-    
+    ans = 0
+    for i in data:
+        if i.isdigit():
+            ans += int(i)
+    return ans
+
+f = open('txt_file/data07.txt').read()
+print(main(f))   
 # Read data from file
