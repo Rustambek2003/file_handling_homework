426fae77f8cb742961760def084505e47ba988ad
rustambekjumaqulov@gmail.com <rustambekjumaqulov@gmail.com>
2022-11-10 14:49:28 +0500

    '10'

--- file10.py
+++ file10.py
@@ -6,5 +6,15 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    data = data.split('\n')
+    ans = len(data[0])
+    print(data)
+    for i in data:
+        
+        if ans < len(i):
+            ans = len(i)
+    return ans
+f = open('txt_file/data10.txt').read()
+print(main(f))
 
 # Read data from file
