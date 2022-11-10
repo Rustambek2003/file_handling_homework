a29131f73b8403eec9c961914c42543761ca69fd
rustambekjumaqulov@gmail.com <rustambekjumaqulov@gmail.com>
2022-11-10 14:41:03 +0500

    '09'

--- file09.py
+++ file09.py
@@ -6,5 +6,12 @@ def main(data:str):
     Returns:
         int: return answer
     """
-
+    data = data.split()
+    ans = 0
+    for i in data:
+        if i.isdigit() and ans > int(i):
+            ans = int(i)
+    return ans
+f = open('txt_file/data09.txt').read()
+print(main(f))
 # Read data from file
